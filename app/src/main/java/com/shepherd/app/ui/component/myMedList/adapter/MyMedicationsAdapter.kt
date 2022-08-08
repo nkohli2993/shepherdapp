5ecb1965f65b3b782d9eb6098d129980b09e058b
Nikita <nikita.kohli@itechnolabs.tech>
2022-08-08 11:08:26 +0530

    Med List Screen - UI/UX for popup for edit and Delete

--- app/src/main/java/com/shepherd/app/ui/component/myMedList/adapter/MyMedicationsAdapter.kt
+++ app/src/main/java/com/shepherd/app/ui/component/myMedList/adapter/MyMedicationsAdapter.kt
@@ -1,14 +1,21 @@
 package com.shepherd.app.ui.component.myMedList.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.graphics.Rect
+import android.os.Build
+import android.view.*
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.shepherd.app.R
 import com.shepherd.app.data.dto.med_list.loved_one_med_list.Medlists
 import com.shepherd.app.databinding.AdapterMyMedicationsListBinding
 import com.shepherd.app.ui.base.listeners.RecyclerItemListener
+import com.shepherd.app.utils.extensions.showInfo
 import com.shepherd.app.view_model.MyMedListViewModel
 
 
@@ -61,6 +68,7 @@ class MyMedicationsAdapter(
                 )
             }
             itemBinding.imgMore.setOnClickListener {
+                showPopupReportPost(context,itemBinding.imgMore)
                 openEditOption(
                     absoluteAdapterPosition,
                     itemBinding.imgMore,
@@ -111,4 +119,76 @@ class MyMedicationsAdapter(
         notifyDataSetChanged()
     }
 
+    private fun popupLocateView(v: View?): Rect? {
+        val loc_int = IntArray(2)
+        if (v == null) return null
+        try {
+            v.getLocationOnScreen(loc_int)
+        } catch (npe: NullPointerException) {
+            //Happens when the view doesn't exist on screen anymore.
+            return null
+        }
+        val location = Rect()
+        location.left = loc_int[0]
+        location.top = loc_int[1]
+        location.right = location.left + v.width
+        location.bottom = location.top + v.height
+        return location
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun showPopupReportPost(
+        context: Context,
+        view: View?,
+    ) {
+        val popupView: View = LayoutInflater.from(context).inflate(
+            R.layout.popup_report,
+            null
+        )
+        val width: Int = LinearLayout.LayoutParams.WRAP_CONTENT
+        val height: Int = LinearLayout.LayoutParams.WRAP_CONTENT
+        val focusable = true // lets taps outside the popup also dismiss it
+        val popupWindow = PopupWindow(popupView, width, height, focusable)
+        val editTV: AppCompatTextView = popupView.findViewById(R.id.editTV) as AppCompatTextView
+        val deleteTV: AppCompatTextView = popupView.findViewById(R.id.deleteTV) as AppCompatTextView
+        popupWindow.showAtLocation(
+            popupView,
+            Gravity.TOP or Gravity.LEFT,
+            popupLocateView(view)?.left!!,
+            popupLocateView(view)?.top!!
+        )
+        // show the dim background
+        val container: View
+        container = if (popupWindow.getBackground() == null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                popupWindow.contentView.parent as View
+            } else {
+                popupWindow.contentView
+            }
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                popupWindow.contentView.parent.parent as View
+            } else {
+                popupWindow.contentView.parent as View
+            }
+        }
+        val context: Context = popupWindow.getContentView().getContext()
+        val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val p = container.layoutParams as WindowManager.LayoutParams
+        p.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND
+        p.dimAmount = 0.6f
+        wm.updateViewLayout(container, p)
+        popupView.setOnTouchListener { v, event ->
+            popupWindow.dismiss()
+            true
+        }
+
+        editTV.setOnClickListener {
+            showInfo(context,"Edit")
+        }
+        deleteTV.setOnClickListener {
+            showInfo(context,"delete")
+        }
+
+    }
 }
