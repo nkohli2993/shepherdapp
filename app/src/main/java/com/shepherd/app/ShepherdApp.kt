bea6fac36fce06fcce16bee52edff041f2c5da44
Deepak Rattan <deepak.rattan@itechnolabs.tech>
2022-08-12 10:36:21 +0530

    Adds dependencies for Firebase firestore -> Initializes FirebaseFireStore in Application

--- app/src/main/java/com/shepherd/app/ShepherdApp.kt
+++ app/src/main/java/com/shepherd/app/ShepherdApp.kt
@@ -1,6 +1,10 @@
 package com.shepherd.app
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
 
 /**
@@ -10,10 +14,13 @@ import dagger.hilt.android.HiltAndroidApp
 open class ShepherdApp : Application() {
     companion object {
         var appContext: Application? = null
+        lateinit var db: FirebaseFirestore
     }
 
     override fun onCreate() {
         super.onCreate()
         appContext = this
+        FirebaseApp.initializeApp(this)
+        db = Firebase.firestore
     }
 }
