164bda9c8ac6065947ecd32d339bda1212062370
Deepak Rattan <deepak.rattan@itechnolabs.tech>
2022-10-29 16:06:07 +0530

    Add VitalStats Screen : Makes the vitalStat optional and adds validation for adding at least one of the vital stat

--- app/src/main/java/com/shepherdapp/app/ui/component/add_vitals/AddVitalsFragment.kt
+++ app/src/main/java/com/shepherdapp/app/ui/component/add_vitals/AddVitalsFragment.kt
@@ -18,7 +18,9 @@ import com.shepherdapp.app.databinding.FragmentAddVitalsBinding
 import com.shepherdapp.app.network.retrofit.DataResult
 import com.shepherdapp.app.network.retrofit.observeEvent
 import com.shepherdapp.app.ui.base.BaseFragment
-import com.shepherdapp.app.utils.extensions.*
+import com.shepherdapp.app.utils.extensions.showError
+import com.shepherdapp.app.utils.extensions.showInfo
+import com.shepherdapp.app.utils.extensions.showSuccess
 import com.shepherdapp.app.view_model.AddNewVitalStatsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
@@ -30,6 +32,15 @@ class AddVitalsFragment : BaseFragment<FragmentAddVitalsBinding>(), View.OnClick
     private lateinit var fragmentAddVitalsBinding: FragmentAddVitalsBinding
     private val addVitalStatsViewModel: AddNewVitalStatsViewModel by viewModels()
     private var isAmPm: String? = null
+
+    var heartRate: String? = null
+    var bodyTemp: String? = null
+    var oxygen: String? = null
+    var sBP: String? = null
+    var dBP: String? = null
+    var data: AddVitalData? = null
+    var bloodPressureData: AddBloodPressureData? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -109,27 +120,68 @@ class AddVitalsFragment : BaseFragment<FragmentAddVitalsBinding>(), View.OnClick
 
             R.id.btnAdd -> {
                 if (isValid) {
-                    var selectedDate = fragmentAddVitalsBinding.tvDate.text.toString().trim()
-                    var dateFormat = SimpleDateFormat("dd-MM-yyyy")
-                    val formattedDate: Date = dateFormat.parse(selectedDate)!!
-                    dateFormat = SimpleDateFormat("yyyy-MM-dd")
-                    selectedDate = dateFormat.format(formattedDate)
-                    val data = AddVitalData(
-                        fragmentAddVitalsBinding.etHeartRate.text.toString().trim(),
-                        AddBloodPressureData(
-                            fragmentAddVitalsBinding.etSbp.text.toString().trim(),
-                            fragmentAddVitalsBinding.etDbp.text.toString().trim()
-                        ),
-                        fragmentAddVitalsBinding.etTemp.text.toString().trim(),
-                        fragmentAddVitalsBinding.etSpo.text.toString().trim()
-                    )
-                    val stats = VitalStatsRequestModel(
-                        addVitalStatsViewModel.getLovedOneUUId(),
-                        selectedDate,
-                        fragmentAddVitalsBinding.tvTime.text.toString().plus(" $isAmPm"),
-                        data
-                    )
-                    addVitalStatsViewModel.addVitalStats(stats)
+                    heartRate = fragmentAddVitalsBinding.etHeartRate.text.toString().trim()
+                    bodyTemp = fragmentAddVitalsBinding.etTemp.text.toString().trim()
+                    oxygen = fragmentAddVitalsBinding.etSpo.text.toString().trim()
+                    sBP = fragmentAddVitalsBinding.etSbp.text.toString().trim()
+                    dBP = fragmentAddVitalsBinding.etDbp.text.toString().trim()
+
+                    if (heartRate.isNullOrEmpty() && bodyTemp.isNullOrEmpty() && oxygen.isNullOrEmpty() && sBP.isNullOrEmpty() && dBP.isNullOrEmpty()) {
+                        showInfo(requireContext(), "Please select at least one of the VitalStat...")
+                    } else {
+                        var selectedDate = fragmentAddVitalsBinding.tvDate.text.toString().trim()
+                        var dateFormat = SimpleDateFormat("dd-MM-yyyy")
+                        val formattedDate: Date = dateFormat.parse(selectedDate)!!
+                        dateFormat = SimpleDateFormat("yyyy-MM-dd")
+                        selectedDate = dateFormat.format(formattedDate)
+
+                        if (heartRate.isNullOrEmpty()) {
+                            heartRate = null
+                        }
+                        if (bodyTemp.isNullOrEmpty()) {
+                            bodyTemp = null
+                        }
+                        if (oxygen.isNullOrEmpty()) {
+                            oxygen = null
+                        }
+                        if (sBP.isNullOrEmpty()) {
+                            sBP = null
+                        }
+                        if (dBP.isNullOrEmpty()) {
+                            dBP = null
+                        }
+
+                        bloodPressureData = if (sBP.isNullOrEmpty() && dBP.isNullOrEmpty()) {
+                            null
+                        } else {
+                            AddBloodPressureData(sbp = sBP, dbp = dBP)
+                        }
+                        val data = AddVitalData(
+                            heartRate = heartRate,
+                            bloodPressure = bloodPressureData,
+                            bodyTemp = bodyTemp,
+                            oxygen = oxygen
+                        )
+
+                        /*  val data = AddVitalData(
+                              fragmentAddVitalsBinding.etHeartRate.text.toString().trim(),
+                              AddBloodPressureData(
+                                  fragmentAddVitalsBinding.etSbp.text.toString().trim(),
+                                  fragmentAddVitalsBinding.etDbp.text.toString().trim()
+                              ),
+                              fragmentAddVitalsBinding.etTemp.text.toString().trim(),
+                              fragmentAddVitalsBinding.etSpo.text.toString().trim()
+                          )*/
+                        val stats = VitalStatsRequestModel(
+                            addVitalStatsViewModel.getLovedOneUUId(),
+                            selectedDate,
+                            fragmentAddVitalsBinding.tvTime.text.toString().plus(" $isAmPm"),
+                            data
+                        )
+                        addVitalStatsViewModel.addVitalStats(stats)
+                    }
+
+
                 }
             }
         }
@@ -214,83 +266,83 @@ class AddVitalsFragment : BaseFragment<FragmentAddVitalsBinding>(), View.OnClick
                     fragmentAddVitalsBinding.tvTime.error = getString(R.string.please_choose_time)
                     fragmentAddVitalsBinding.tvTime.requestFocus()
                 }
-                fragmentAddVitalsBinding.etHeartRate.text.toString().trim().isEmpty() -> {
-                    fragmentAddVitalsBinding.etHeartRate.error =
-                        getString(R.string.please_enter_your_heart_rate)
-                    fragmentAddVitalsBinding.etHeartRate.requestFocus()
-                }
-                fragmentAddVitalsBinding.etHeartRate.text.toString().toDouble() < 60 -> {
-                    fragmentAddVitalsBinding.etHeartRate.error =
-                        getString(R.string.please_enter_valid_heart_rate)
-                    fragmentAddVitalsBinding.etHeartRate.requestFocus()
-                }
-                fragmentAddVitalsBinding.etHeartRate.text.toString().toDouble() > 100 -> {
-                    fragmentAddVitalsBinding.etHeartRate.error =
-                        getString(R.string.please_enter_valid_heart_rate)
-                    fragmentAddVitalsBinding.etHeartRate.requestFocus()
-                }
-                fragmentAddVitalsBinding.etTemp.text.toString().trim().isEmpty() -> {
-                    fragmentAddVitalsBinding.etTemp.error = getString(R.string.please_choose_temp)
-                    fragmentAddVitalsBinding.etTemp.requestFocus()
-                }
-                fragmentAddVitalsBinding.etTemp.checkString().toDouble() < 95.9 -> {
-                    fragmentAddVitalsBinding.etTemp.error =
-                        getString(R.string.please_enter_valid_body_temperature)
-                    fragmentAddVitalsBinding.etTemp.requestFocus()
-                }
-                fragmentAddVitalsBinding.etTemp.checkString().toDouble() > 105 -> {
-                    fragmentAddVitalsBinding.etTemp.error =
-                        getString(R.string.please_enter_valid_body_temperature)
-                    fragmentAddVitalsBinding.etTemp.requestFocus()
-                }
-                fragmentAddVitalsBinding.etSbp.text.toString().trim().isEmpty() -> {
-                    fragmentAddVitalsBinding.etSbp.error =
-                        getString(R.string.please_enter_your_blood_pressure_sbp)
-                    fragmentAddVitalsBinding.etSbp.requestFocus()
-                }
-                fragmentAddVitalsBinding.etSbp.checkString().toDouble() < 60 -> {
-                    fragmentAddVitalsBinding.etSbp.error =
-                        getString(R.string.please_enter_valid_sbp_bp)
-                    fragmentAddVitalsBinding.etSbp.requestFocus()
-                }
-                fragmentAddVitalsBinding.etSbp.checkString().toDouble() > 180 -> {
-                    fragmentAddVitalsBinding.etSbp.error =
-                        getString(R.string.please_enter_valid_sbp_bp)
-                    fragmentAddVitalsBinding.etSbp.requestFocus()
-                }
-                fragmentAddVitalsBinding.etDbp.text.toString().trim().isEmpty() -> {
-                    fragmentAddVitalsBinding.etDbp.error =
-                        getString(R.string.please_enter_your_blood_pressure_dbp)
-                    fragmentAddVitalsBinding.etDbp.requestFocus()
-                }
-                fragmentAddVitalsBinding.etDbp.checkString().toDouble() < 60 -> {
-                    fragmentAddVitalsBinding.etDbp.error =
-                        getString(R.string.please_enter_valid_dbp_bp)
-                    fragmentAddVitalsBinding.etDbp.requestFocus()
-                }
-                fragmentAddVitalsBinding.etDbp.checkString().toDouble() > 180 -> {
-                    fragmentAddVitalsBinding.etDbp.error =
-                        getString(R.string.please_enter_valid_dbp_bp)
-                    fragmentAddVitalsBinding.etDbp.requestFocus()
-                }
-                fragmentAddVitalsBinding.etSpo.text.toString().trim().isEmpty() -> {
-                    fragmentAddVitalsBinding.etSpo.error =
-                        getString(R.string.please_enter_body_oxygen)
-                    fragmentAddVitalsBinding.etSpo.requestFocus()
-                }
-                fragmentAddVitalsBinding.etSpo.checkString()
-                    .isNotEmpty() && fragmentAddVitalsBinding.etSpo.checkString()
-                    .toInt() <= 0 && fragmentAddVitalsBinding.etSpo.checkString().toInt() > 100 -> {
-                    fragmentAddVitalsBinding.etSpo.error =
-                        getString(R.string.please_enter_valid_oxygen_level)
-                    fragmentAddVitalsBinding.etSpo.requestFocus()
-                }
-                fragmentAddVitalsBinding.etSpo.checkString()
-                    .isNotEmpty() && fragmentAddVitalsBinding.etSpo.checkString().toInt() > 100 -> {
-                    fragmentAddVitalsBinding.etSpo.error =
-                        getString(R.string.please_enter_valid_oxygen_level)
-                    fragmentAddVitalsBinding.etSpo.requestFocus()
-                }
+                /*          fragmentAddVitalsBinding.etHeartRate.text.toString().trim().isEmpty() -> {
+                              fragmentAddVitalsBinding.etHeartRate.error =
+                                  getString(R.string.please_enter_your_heart_rate)
+                              fragmentAddVitalsBinding.etHeartRate.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etHeartRate.text.toString().toDouble() < 60 -> {
+                              fragmentAddVitalsBinding.etHeartRate.error =
+                                  getString(R.string.please_enter_valid_heart_rate)
+                              fragmentAddVitalsBinding.etHeartRate.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etHeartRate.text.toString().toDouble() > 120 -> {
+                              fragmentAddVitalsBinding.etHeartRate.error =
+                                  getString(R.string.please_enter_valid_heart_rate)
+                              fragmentAddVitalsBinding.etHeartRate.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etTemp.text.toString().trim().isEmpty() -> {
+                              fragmentAddVitalsBinding.etTemp.error = getString(R.string.please_choose_temp)
+                              fragmentAddVitalsBinding.etTemp.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etTemp.checkString().toDouble() < 95 -> {
+                              fragmentAddVitalsBinding.etTemp.error =
+                                  getString(R.string.please_enter_valid_body_temperature)
+                              fragmentAddVitalsBinding.etTemp.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etTemp.checkString().toDouble() > 106 -> {
+                              fragmentAddVitalsBinding.etTemp.error =
+                                  getString(R.string.please_enter_valid_body_temperature)
+                              fragmentAddVitalsBinding.etTemp.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etSbp.text.toString().trim().isEmpty() -> {
+                              fragmentAddVitalsBinding.etSbp.error =
+                                  getString(R.string.please_enter_your_blood_pressure_sbp)
+                              fragmentAddVitalsBinding.etSbp.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etSbp.checkString().toDouble() < 40 -> {
+                              fragmentAddVitalsBinding.etSbp.error =
+                                  getString(R.string.please_enter_valid_sbp_bp)
+                              fragmentAddVitalsBinding.etSbp.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etSbp.checkString().toDouble() > 300 -> {
+                              fragmentAddVitalsBinding.etSbp.error =
+                                  getString(R.string.please_enter_valid_sbp_bp)
+                              fragmentAddVitalsBinding.etSbp.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etDbp.text.toString().trim().isEmpty() -> {
+                              fragmentAddVitalsBinding.etDbp.error =
+                                  getString(R.string.please_enter_your_blood_pressure_dbp)
+                              fragmentAddVitalsBinding.etDbp.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etDbp.checkString().toDouble() < 40 -> {
+                              fragmentAddVitalsBinding.etDbp.error =
+                                  getString(R.string.please_enter_valid_dbp_bp)
+                              fragmentAddVitalsBinding.etDbp.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etDbp.checkString().toDouble() > 300 -> {
+                              fragmentAddVitalsBinding.etDbp.error =
+                                  getString(R.string.please_enter_valid_dbp_bp)
+                              fragmentAddVitalsBinding.etDbp.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etSpo.text.toString().trim().isEmpty() -> {
+                              fragmentAddVitalsBinding.etSpo.error =
+                                  getString(R.string.please_enter_body_oxygen)
+                              fragmentAddVitalsBinding.etSpo.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etSpo.checkString()
+                              .isNotEmpty() && fragmentAddVitalsBinding.etSpo.checkString()
+                              .toInt() <= 0 && fragmentAddVitalsBinding.etSpo.checkString().toInt() > 100 -> {
+                              fragmentAddVitalsBinding.etSpo.error =
+                                  getString(R.string.please_enter_valid_oxygen_level)
+                              fragmentAddVitalsBinding.etSpo.requestFocus()
+                          }
+                          fragmentAddVitalsBinding.etSpo.checkString()
+                              .isNotEmpty() && fragmentAddVitalsBinding.etSpo.checkString().toInt() > 100 -> {
+                              fragmentAddVitalsBinding.etSpo.error =
+                                  getString(R.string.please_enter_valid_oxygen_level)
+                              fragmentAddVitalsBinding.etSpo.requestFocus()
+                          }*/
                 else -> {
                     return true
                 }
